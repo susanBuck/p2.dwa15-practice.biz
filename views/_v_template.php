6b18fee7de519320951b1aae2f01641f4e506a30
Susan Buck <susan@susanbuck.net>
2013-10-23 16:23:38 -0400

    Simple menu.

--- views/_v_template.php
+++ views/_v_template.php
@@ -9,12 +9,27 @@
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>				
 										
 	<!-- Controller Specific JS/CSS -->
+	<link rel="stylesheet" href="/css/sample-app.css" type="text/css">
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 		
 </head>
 
 <body>	
 
+	<nav>
+		<menu>
+			<?php if($user): ?>
+				<li><a href='/posts/add'>Add Post</a></li>
+				<li><a href='/posts/'>View Posts</a></li>
+				<li><a href='/posts/users'>Follow users</a></li>
+				<li><a href='/users/logout'>Logout</a></li>
+			<?php else: ?>
+				<li><a href='/users/signup'>Sign up</a></li>
+				<li><a href='/users/login'>Log in</a></li>
+			<?php endif; ?>
+		</menu>
+	</nav>
+	
 	<?php if(isset($content)) echo $content; ?>
 
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
